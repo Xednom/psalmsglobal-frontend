f9b948157bc64da582e9aff94c6f40367d2435de
Xednom <monde.lacanlalay@gmail.com>
2021-06-09 09:06:34 +0800

    removed search box in navbar

--- components/layouts/argon/DashboardNavbar.vue
+++ components/layouts/argon/DashboardNavbar.vue
@@ -4,33 +4,6 @@
     class="navbar-top border-bottom navbar-expand"
     :class="{ 'bg-success navbar-dark': type === 'default' }"
   >
-    <!-- Search form -->
-    <form
-      class="navbar-search form-inline mr-sm-3"
-      :class="{
-        'navbar-search-light': type === 'default',
-        'navbar-search-dark': type === 'light'
-      }"
-      id="navbar-search-main"
-    >
-      <div class="form-group mb-0">
-        <div class="input-group input-group-alternative input-group-merge">
-          <div class="input-group-prepend">
-            <span class="input-group-text"><i class="fas fa-search"></i></span>
-          </div>
-          <input class="form-control" placeholder="Search" type="text" />
-        </div>
-      </div>
-      <button
-        type="button"
-        class="close"
-        data-action="search-close"
-        data-target="#navbar-search-main"
-        aria-label="Close"
-      >
-        <span aria-hidden="true">×</span>
-      </button>
-    </form>
     <!-- Navbar links -->
     <ul class="navbar-nav align-items-center ml-md-auto">
       <li class="nav-item d-xl-none">
@@ -61,226 +34,6 @@
           <i class="ni ni-zoom-split-in"></i>
         </a>
       </li>
-      <base-dropdown
-        class="nav-item"
-        tag="li"
-        title-classes="nav-link"
-        title-tag="a"
-        icon="ni ni-bell-55"
-        menu-classes="dropdown-menu-xl dropdown-menu-right py-0 overflow-hidden"
-      >
-        <template>
-          <!-- Dropdown header -->
-          <div class="px-3 py-3">
-            <h6 class="text-sm text-muted m-0">
-              You have <strong class="text-primary">13</strong> notifications.
-            </h6>
-          </div>
-          <!-- List group -->
-          <div class="list-group list-group-flush">
-            <a href="#!" class="list-group-item list-group-item-action">
-              <div class="row align-items-center">
-                <div class="col-auto">
-                  <!-- Avatar -->
-                  <img
-                    alt="Image placeholder"
-                    src="img/theme/team-1.jpg"
-                    class="avatar rounded-circle"
-                  />
-                </div>
-                <div class="col ml--2">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <h4 class="mb-0 text-sm">John Snow</h4>
-                    </div>
-                    <div class="text-right text-muted">
-                      <small>2 hrs ago</small>
-                    </div>
-                  </div>
-                  <p class="text-sm mb-0">
-                    Let's meet at Starbucks at 11:30. Wdyt?
-                  </p>
-                </div>
-              </div>
-            </a>
-            <a href="#!" class="list-group-item list-group-item-action">
-              <div class="row align-items-center">
-                <div class="col-auto">
-                  <!-- Avatar -->
-                  <img
-                    alt="Image placeholder"
-                    src="img/theme/team-2.jpg"
-                    class="avatar rounded-circle"
-                  />
-                </div>
-                <div class="col ml--2">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <h4 class="mb-0 text-sm">John Snow</h4>
-                    </div>
-                    <div class="text-right text-muted">
-                      <small>3 hrs ago</small>
-                    </div>
-                  </div>
-                  <p class="text-sm mb-0">
-                    A new issue has been reported for Argon.
-                  </p>
-                </div>
-              </div>
-            </a>
-            <a href="#!" class="list-group-item list-group-item-action">
-              <div class="row align-items-center">
-                <div class="col-auto">
-                  <!-- Avatar -->
-                  <img
-                    alt="Image placeholder"
-                    src="img/theme/team-3.jpg"
-                    class="avatar rounded-circle"
-                  />
-                </div>
-                <div class="col ml--2">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <h4 class="mb-0 text-sm">John Snow</h4>
-                    </div>
-                    <div class="text-right text-muted">
-                      <small>5 hrs ago</small>
-                    </div>
-                  </div>
-                  <p class="text-sm mb-0">Your posts have been liked a lot.</p>
-                </div>
-              </div>
-            </a>
-            <a href="#!" class="list-group-item list-group-item-action">
-              <div class="row align-items-center">
-                <div class="col-auto">
-                  <!-- Avatar -->
-                  <img
-                    alt="Image placeholder"
-                    src="img/theme/team-4.jpg"
-                    class="avatar rounded-circle"
-                  />
-                </div>
-                <div class="col ml--2">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <h4 class="mb-0 text-sm">John Snow</h4>
-                    </div>
-                    <div class="text-right text-muted">
-                      <small>2 hrs ago</small>
-                    </div>
-                  </div>
-                  <p class="text-sm mb-0">
-                    Let's meet at Starbucks at 11:30. Wdyt?
-                  </p>
-                </div>
-              </div>
-            </a>
-            <a href="#!" class="list-group-item list-group-item-action">
-              <div class="row align-items-center">
-                <div class="col-auto">
-                  <!-- Avatar -->
-                  <img
-                    alt="Image placeholder"
-                    src="img/theme/team-5.jpg"
-                    class="avatar rounded-circle"
-                  />
-                </div>
-                <div class="col ml--2">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div>
-                      <h4 class="mb-0 text-sm">John Snow</h4>
-                    </div>
-                    <div class="text-right text-muted">
-                      <small>3 hrs ago</small>
-                    </div>
-                  </div>
-                  <p class="text-sm mb-0">
-                    A new issue has been reported for Argon.
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
-          <!-- View all -->
-          <a
-            href="#!"
-            class="dropdown-item text-center text-primary font-weight-bold py-3"
-            >View all</a
-          >
-        </template>
-      </base-dropdown>
-      <base-dropdown
-        menu-classes="dropdown-menu-lg dropdown-menu-dark bg-default dropdown-menu-right"
-        class="nav-item"
-        tag="li"
-        title-tag="a"
-        title-classes="nav-link"
-        icon="ni ni-ungroup"
-      >
-        <template>
-          <div class="row shortcuts px-4">
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-red"
-              >
-                <i class="ni ni-calendar-grid-58"></i>
-              </span>
-              <small>Calendar</small>
-            </a>
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-orange"
-              >
-                <i class="ni ni-email-83"></i>
-              </span>
-              <small>Email</small>
-            </a>
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-info"
-              >
-                <i class="ni ni-credit-card"></i>
-              </span>
-              <small>Payments</small>
-            </a>
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-green"
-              >
-                <i class="ni ni-books"></i>
-              </span>
-              <small>Reports</small>
-            </a>
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-purple"
-              >
-                <i class="ni ni-pin-3"></i>
-              </span>
-              <small>Maps</small>
-            </a>
-            <a href="#!" class="col-4 shortcut-item">
-              <span
-                class="shortcut-media avatar rounded-circle bg-gradient-yellow"
-              >
-                <i class="ni ni-basket"></i>
-              </span>
-              <small>Shop</small>
-            </a>
-          </div>
-        </template>
-      </base-dropdown>
     </ul>
     <ul class="navbar-nav align-items-center ml-auto ml-md-0">
       <base-dropdown
@@ -296,7 +49,8 @@
               <!-- <img alt="Image placeholder" src="img/theme/team-4.jpg" /> -->
             </span>
             <div class="media-body ml-2 d-none d-lg-block">
-              <span class="mb-0 text-sm  font-weight-bold">{{ this.$auth.user.first_name }} 
+              <span class="mb-0 text-sm  font-weight-bold"
+                >{{ this.$auth.user.first_name }}
                 {{ this.$auth.user.last_name }}
               </span>
             </div>
@@ -311,18 +65,6 @@
             <i class="ni ni-single-02"></i>
             <span>My profile</span>
           </nuxt-link>
-          <a href="#!" class="dropdown-item">
-            <i class="ni ni-settings-gear-65"></i>
-            <span>Settings</span>
-          </a>
-          <a href="#!" class="dropdown-item">
-            <i class="ni ni-calendar-grid-58"></i>
-            <span>Activity</span>
-          </a>
-          <a href="#!" class="dropdown-item">
-            <i class="ni ni-support-16"></i>
-            <span>Support</span>
-          </a>
           <div class="dropdown-divider"></div>
           <a href="#!" class="dropdown-item" @click="logout">
             <i class="ni ni-user-run"></i>
