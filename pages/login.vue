1b85c16511697c85bfb5e27b87bd4eb2e627d862
Xednom <monde.lacanlalay@gmail.com>
2021-06-15 17:00:15 +0800

    changed the color scheme for login page background

--- pages/login.vue
+++ pages/login.vue
@@ -3,19 +3,7 @@
     <!-- Header -->
     <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
       <div class="separator separator-bottom separator-skew zindex-100">
-        <svg
-          x="0"
-          y="0"
-          viewBox="0 0 2560 100"
-          preserveAspectRatio="none"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <polygon
-            class="fill-default"
-            points="2560 0 2560 100 0 100"
-          ></polygon>
-        </svg>
+        
       </div>
     </div>
     <!-- Page content -->
@@ -142,3 +130,13 @@ export default {
   }
 };
 </script>
+
+
+<style lang="scss">
+.bg-gradient-success {
+  background: linear-gradient(87deg,#2309fb,#e60a0a) !important;
+}
+.bg-default {
+  background: #ffffff !important;
+}
+</style>
