88802f34943417b2c335c6a981ec5c081c014b53
Xednom <monde.lacanlalay@gmail.com>
2021-08-12 16:31:01 +0800

    changed the avatar to the site logo

--- components/layouts/argon/DashboardNavbar.vue
+++ components/layouts/argon/DashboardNavbar.vue
@@ -46,7 +46,10 @@
         <a href="#" class="nav-link pr-0" @click.prevent slot="title-container">
           <div class="media align-items-center">
             <span class="avatar avatar-sm rounded-circle">
-              <!-- <img alt="Image placeholder" src="img/theme/team-4.jpg" /> -->
+              <img
+                src="~/static/pg.png"
+                alt="Logo white"
+              />
             </span>
             <div class="media-body ml-2 d-none d-lg-block">
               <span class="mb-0 text-sm  font-weight-bold"
