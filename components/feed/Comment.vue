85ece78bfdafd56e13308deefcb8ce60f4ca1324
Xednom <monde.lacanlalay@gmail.com>
2022-01-28 14:58:35 +0800

    hotfix: updated the component comment.vue

--- components/feed/Comment.vue
+++ components/feed/Comment.vue
@@ -3,8 +3,13 @@
     <!-- <img alt="Image placeholder" class="avatar avatar-lg media-comment-avatar rounded-circle" :src="userImage"> -->
     <div class="media-body">
       <div class="media-comment-text">
-        <h6 class="h5 mt-0">{{userName}}</h6>
-        <p class="text-sm lh-160" v-html="text"></p>
+        <h6 class="h5 mt-0">{{ userName }} said</h6>
+        <blockquote class="blockquote">
+          <p class="text-sm lh-160" v-html="text"></p>
+          <footer class="blockquote-footer">
+            commented at <strong>{{createdAt}}</strong>
+          </footer>
+        </blockquote>
         <!-- todo: might need in the future -->
         <!-- <div class="icon-actions">
           <a href="#" class="like active">
@@ -21,30 +26,33 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'comment',
-    props: {
-      userImage: {
-        type: String,
-        default: 'img/theme/team-1.jpg'
-      },
-      userName: {
-        type: String,
-      },
-      text: {
-        type: String,
-        default: 'Cras sit amet nibh libero nulla vel metus scelerisque ante sollicitudin. Cras purus odio vestibulum in vulputate viverra turpis.'
-      },
-      likeCount: {
-        type: Number,
-        default: 0
-      },
-      shareCount: {
-        type: Number,
-        default: 0
-      }
+export default {
+  name: "comment",
+  props: {
+    userImage: {
+      type: String,
+      default: "img/theme/team-1.jpg"
+    },
+    userName: {
+      type: String
+    },
+    text: {
+      type: String,
+      default:
+        "Cras sit amet nibh libero nulla vel metus scelerisque ante sollicitudin. Cras purus odio vestibulum in vulputate viverra turpis."
+    },
+    likeCount: {
+      type: Number,
+      default: 0
+    },
+    shareCount: {
+      type: Number,
+      default: 0
+    },
+    createdAt: {
+      type: String,
     }
   }
+};
 </script>
-<style>
-</style>
+<style></style>
